30d7e1eca424683f1a5a4b408b27b4a4af142da4
Anthony Dinh <anthony.dinh@brooks.com>
2022-12-01 14:19:33 -0800

    part2 for day 1

--- Anthony/day1.kt
+++ Anthony/day1.kt
@@ -2,23 +2,48 @@
 import java.io.File
 
 
-fun main() {
+fun part1() {
     val text = File("day1input.txt").readText()
     val textArray = text.split("\n").map {it.trim() }
     val calorieList = mutableListOf<Int>()
-    
+
     var sum = 0
     for (item in textArray) {
         if (item.isEmpty() ) {
 
             calorieList.add(sum)
             sum = 0
-        } 
+        }
         else {
             sum += item.toInt()
         }
     }
 
     println(calorieList.maxOrNull()) //70613
+}
 
+fun part2() {
+    val text = File("day1input.txt").readText()
+    val textArray = text.split("\n").map {it.trim() }
+    val calorieList = mutableListOf<Int>()
+
+    var sum = 0
+    for (item in textArray) {
+        if (item.isEmpty() ) {
+
+            calorieList.add(sum)
+            sum = 0
+        }
+        else {
+            sum += item.toInt()
+        }
+    }
+
+    calorieList.sortDescending()
+    println(calorieList[0] + calorieList[1] + calorieList[2])
+}
+
+fun main() {
+    part1()
+    part2()
 }
