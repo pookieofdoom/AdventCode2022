a2848e2b168a7c1147e38672ff20c6791d62cf25
Anthony Dinh <anthony.dinh@brooks.com>
2022-12-05 18:29:47 -0800

    part1 for day 3

---
+++ Anthony/day3/day3.kt
@@ -0,0 +1,47 @@
+package day3
+
+import java.io.File
+
+fun day1(){
+    val path = System.getProperty("user.dir")
+    var total = 0
+    val text = File("$path/day3/day3input.txt").readLines().forEach{
+        total += convertToPriority(duplicateCharacters(it))
+    }
+    println(total)
+}
+
+
+fun duplicateCharacters(text: String) : Char {
+
+    val item1List = mutableListOf<Char>()
+    for (i in 0 until text.length / 2) {
+        item1List.add(text[i])
+    }
+
+    for (i in text.length / 2 until text.length) {
+        if (item1List.contains(text[i])) {
+            return text[i]
+        }
+    }
+    return '?'
+
+}
+
+fun convertToPriority(ch : Char) : Int {
+    val MAGIC_ASCII_UPPERCASE = 38
+    val MAGIC_ASCII_LOWERCASE = 96
+    var value = 0
+    value = if (ch.isUpperCase()) {
+        ch.code - MAGIC_ASCII_UPPERCASE
+    } else {
+        ch.code - MAGIC_ASCII_LOWERCASE
+    }
+
+    return value
+}
+
+fun main() {
+    day1()
+    // day2()
+}
