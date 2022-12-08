63dd365ea501ac387127260aab5857e54fd6f533
Anthony Dinh <anthony.dinh@brooks.com>
2022-12-08 15:29:19 -0800

    part2 for day 4

--- Anthony/day4/day4.kt
+++ Anthony/day4/day4.kt
@@ -29,9 +29,30 @@ fun isPairContained(pairText: String): Boolean {
 
 
 fun day2() {
-
+    var total = 0
+    val path = System.getProperty("user.dir")
+    val text = File("$path/day4/day4input.txt").readLines().forEach {
+        if (isPairContained2(it)) {
+            total++
+        }
+    }
+    println(total)
 }
 
+fun isPairContained2(pairText: String): Boolean {
+
+    val pairs = pairText.split(",")
+    val numRange1 = pairs[0].split("-")
+    val numRange2 = pairs[1].split("-")
+    val firstElf = (numRange1[0].toInt()..numRange1[1].toInt()).toList()
+    val secondElf = (numRange2[0].toInt()..numRange2[1].toInt()).toList()
+    if (firstElf.any(secondElf::contains)) {
+        return true
+    }
+
+
+    return false
+}
 
 fun main() {
     day1()
