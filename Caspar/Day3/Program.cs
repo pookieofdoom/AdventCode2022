dbf01a2459f71c885030502f57cf7230e89f6ae0
Caspar Hansen <caspar.hansen@Brooks.com>
2022-12-12 16:48:39 -0800

    Add Day 3 solution

---
+++ Caspar/Day3/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] inputs = File.ReadAllLines("input.txt");
+Dictionary<char, int> itemsToPrioritize = new Dictionary<char, int>();
+Dictionary<char, int> badgesToPrioritize = new Dictionary<char, int>();
+List<HashSet<char>> badgeGroup = new List<HashSet<char>>();
+
+foreach (var rucksack in inputs)
+{
+    // Part 1
+    int lineLen = rucksack.Length;
+    HashSet<char> rucksackItems = new HashSet<char>();
+    badgeGroup.Add(new HashSet<char>());
+    for (int i = 0; i < lineLen / 2; i++)
+    {
+        rucksackItems.Add(rucksack[i]);
+        badgeGroup[(badgeGroup.Count-1) % 3].Add(rucksack[i]);
+    }
+    for (int i = lineLen / 2; i < lineLen; i++)
+    {
+        badgeGroup[(badgeGroup.Count-1) % 3].Add(rucksack[i]);
+        if(rucksackItems.Contains(rucksack[i]))
+        {
+            // Could initialize all possible values to 0 to skip this check
+            int n;
+            if (itemsToPrioritize.TryGetValue(rucksack[i], out n))
+                itemsToPrioritize[rucksack[i]]++;
+            else
+                itemsToPrioritize[rucksack[i]] = 1;
+        }
+    }
+
+
+    if (badgeGroup.Count == 3)
+    {
+        badgeGroup[0].IntersectWith(badgeGroup[1]);
+        badgeGroup[0].IntersectWith(badgeGroup[2]);
+        char badge = badgeGroup[0].ElementAt(0);
+        int n;
+        if (badgesToPrioritize.TryGetValue(badge, out n))
+            badgesToPrioritize[badge]++;
+        else
+            badgesToPrioritize[badge] = 1;
+        badgeGroup.Clear();
+    }
+}
+
+int count = 0;
+foreach (var itemKVP in itemsToPrioritize)
+{
+    char item = itemKVP.Key;
+    if (char.IsLower(item))
+        count += ((int)item - (int)'a' + 1) * itemKVP.Value;
+    else
+        count += ((int)item - (int)'A' + 27) * itemKVP.Value;
+}
+System.Console.WriteLine("Part 1: " + count);
+
+int count2 = 0;
+foreach (var itemKVP in badgesToPrioritize)
+{
+    char item = itemKVP.Key;
+    if (char.IsLower(item))
+        count2 += ((int)item - (int)'a' + 1) * itemKVP.Value;
+    else
+        count2 += ((int)item - (int)'A' + 27) * itemKVP.Value;
+}
+System.Console.WriteLine("Part 2: " + count2);
