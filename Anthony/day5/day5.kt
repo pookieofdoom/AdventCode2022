a2445981cf572a58e0eab72d24e59211c217f94b
Anthony Dinh <anthony.dinh@brooks.com>
2022-12-14 17:35:49 -0800

    secondary attempt at  part 1 for day 5

--- Anthony/day5/day5.kt
+++ Anthony/day5/day5.kt
@@ -4,20 +4,22 @@ import day4.isPairContained
 import java.io.File
 import java.util.ArrayDeque
 
-class SupplyStacks {
-    val supplyStacks = listOf<ArrayDeque<Char>>()
-
-
-}
-
 
 fun day1() {
     val path = System.getProperty("user.dir")
     var stackFlag = false
+    val stackList = mutableMapOf<Int, ArrayDeque<Char>>()
     val text = File("$path/day5/day5input.txt").readLines().forEach {
 
         if (!stackFlag and !it.contains("[0-9]".toRegex())) {
-            println(it.split("   "))
+            val splitLine = it.split(" [")
+            for (i in splitLine.indices) {
+                if (!stackList.containsKey(i)) {
+                    stackList[i] // placeholder
+                }
+            }
+
+
         } else {
             if (!stackFlag) {
                 println(it.split("   "))
